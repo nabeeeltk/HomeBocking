eae5a7087a74fd13c41c86c0abb78d576e6d9473
nabeeeltk <nabeelmuhammed406@gmail.com>
2023-09-14 12:51:44 +0530

    added Home details page

---
+++ real_estate/lib/utile/Constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+ const maincolor = Color.fromARGB(255, 2, 48, 85);
+ const khight15= SizedBox(height: 15,);
--- real_estate/lib/veiw/home_details_page.dart
+++ real_estate/lib/veiw/home_details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:real_estate/utile/Constants.dart';
 import 'package:real_estate/widgets/image_container.dart';
 
 class HomeDetailsPage extends StatelessWidget {
@@ -9,88 +10,110 @@ class HomeDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
-        backgroundColor: const Color.fromARGB(255, 2, 48, 85),
-        leading: IconButton(onPressed: (){
-          Get.back();
-        }, icon: Icon(Icons.arrow_back_ios_new,size: 30,color: Colors.white,)),
+        title: const Text(
+          "Details",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: maincolor,
+        leading: IconButton(
+            onPressed: () {
+              Get.back();
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios_new,
+              size: 30,
+              color: Colors.white,
+            )),
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              
               height: 200,
-              
-              decoration:const  BoxDecoration(
-                image: DecorationImage(image: AssetImage("image/hm1.jpeg"),fit: BoxFit.cover)
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("image/hm1.jpeg"), fit: BoxFit.cover)),
+            ),
+         khight15,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: CircleAvatar(
+                    radius: 25,
+                    child: Icon(Icons.person,size: 30,),
+                  ),
+                ),
+                const Text(
+                  "Jhone Abraham",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.call,
+                        size: 30,
+                      )),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.message,
+                        size: 30,
+                      )),
+                ),
+              ],
+            ),
+           khight15,
+            const ImageContainer(),
+            const Padding(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Text(
+                "Rouse Villa House",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
               ),
             ),
-           const  SizedBox(height: 15,),
-            const  ImageContainer(),
-         const   Padding(
-             padding: EdgeInsets.only(left:8.0),
-             child: Text("Rouse Villa House",style: TextStyle(fontSize: 25,
-             fontWeight: FontWeight.bold),),
-           ),
             const Padding(
-             padding:  EdgeInsets.only(left: 8),
-             child: Text("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
-             style: TextStyle(fontSize: 12,
-             ),),
-           ),
-          const  SizedBox(height: 15,),
+              padding: EdgeInsets.only(left: 8),
+              child: Text(
+                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+           khight15,
             Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-            const  Padding(
-               padding:  EdgeInsets.only(left:8.0),
-               child: CircleAvatar(
-                radius: 25,
-               ),
-             ),
-            const  Text("Jhone Abrahm",style: TextStyle(fontSize: 25,
-             fontWeight: FontWeight.bold),),
-              Padding(
-               padding:  EdgeInsets.only(left:8.0),
-               child: CircleAvatar(
-                child: IconButton(onPressed: (){}, icon: const Icon(Icons.call,size: 30,)),
-                radius: 25,
-               ),
-             ),
-
-              Padding(
-               padding: const  EdgeInsets.only(left:8.0),
-               child: CircleAvatar(
-                radius: 25,
-                child: IconButton(onPressed: (){}, icon: const Icon(Icons.message,size: 30,)),
-               ),
-             ),
-
-             
-           ],),
-
-        const    SizedBox(height: 15,),
-           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              MaterialButton(
-                minWidth: 50,
-                height: 40,
-                color: Colors.black,
-                child: const Text("₹ 150000",style: TextStyle(color: Colors.white),),
-                onPressed: (){}),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
                 MaterialButton(
-                color: Colors.black,
-                child: const Text("BUY NOW",style: TextStyle(color: Colors.white),),
-                onPressed: (){}),
-            ],
-           )
+                    minWidth: 50,
+                    height: 40,
+                    color:  maincolor,
+                    child: const Text(
+                      "₹ 150000",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {}),
+                MaterialButton(
+                    color: Colors.black,
+                    child: const Text(
+                      "BUY NOW",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {}),
+              ],
+            )
           ],
-          
-          
-
         ),
       ),
     );
