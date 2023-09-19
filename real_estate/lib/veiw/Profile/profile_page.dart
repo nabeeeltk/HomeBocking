7bf546255bd26a25b416299b55b1b7cb7a24f496
nabeeeltk <nabeelmuhammed406@gmail.com>
2023-09-19 17:17:53 +0530

    addedd profile

--- real_estate/lib/veiw/Profile/profile_page.dart
+++ real_estate/lib/veiw/Profile/profile_page.dart
@@ -10,18 +10,55 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: maincolor,
-        title: Text("Profile ",style: TextStyle(color: Colors.white),),
-        leading: IconButton(onPressed: (){
-          Get.back();
-        }, icon: const Icon(Icons.arrow_back_ios_new,size: 30,
-        color: Colors.white,)),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-
-          ],
+        title: const Text(
+          "Profile",
+          style: TextStyle(color: Colors.white),
         ),
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            size: 30,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 150,
+                  width: 100,
+                  color: Colors.blueGrey,
+                ),
+              ),
+            const   Expanded(
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: ' entername',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 16), // Adjust the spacing as needed
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Enter Adress',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
