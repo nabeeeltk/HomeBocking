0c0a875b8cd3196606959be18d83ab96a5a6e2d9
nabeeeltk <nabeelmuhammed406@gmail.com>
2023-09-20 16:48:42 +0530

    commit

--- real_estate/lib/utile/Constants.dart
+++ real_estate/lib/utile/Constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
  const maincolor = Color.fromARGB(255, 2, 48, 85);
  const khight15= SizedBox(height: 15,);
+ const khight20=SizedBox(height: 20,);
--- real_estate/lib/veiw/home/home_screen.dart
+++ real_estate/lib/veiw/home/home_screen.dart
@@ -6,72 +6,82 @@ class HomeScreenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-     
-      body:  SingleChildScrollView(
+    return Scaffold(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-              const SizedBox(height: 25,),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                
-                  children: [
-                  const   Icon(Icons.place),
-                   Text("Malappuram",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),)
-                  ],
-                ),
-              ),
-
-           const  Padding(
-              padding:  EdgeInsets.all(8.0),
-              child:    Text("Good Mornig..!",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 27, 87, 136)),),
+            const SizedBox(
+              height: 25,
             ),
-
-       const SizedBox(
-          height: 70,
-          child:   Padding(
-              padding:  EdgeInsets.all(8.0),
-              child:    TextField(
-                 
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Icon(Icons.place),
+                  Text(
+                    "Malappuram ,Kerala",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Good Mornig..!",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 27, 87, 136)),
+              ),
+            ),
+            const SizedBox(
+              height: 70,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
                   decoration: InputDecoration(
-                    hintText: "Search Your Faveraite",
-                    suffixIcon: Icon(Icons.search,size: 30,),
-                    fillColor: Colors.blueGrey,
-                    filled: true,
-                     border: OutlineInputBorder(borderSide: BorderSide.none)
-                  ),
-                  
+                      hintText: "Search Your Faveraite",
+                      suffixIcon: Icon(
+                        Icons.search,
+                        size: 30,
+                      ),
+                      fillColor: Colors.blueGrey,
+                      filled: true,
+                      border: OutlineInputBorder(borderSide: BorderSide.none)),
                 ),
-            ),
-        ),
-
-          Card( 
-            
-            color: Colors.black,
-            child: Container(
-              height: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadiusDirectional.circular(10),
-                image:const  DecorationImage(image: AssetImage("image/home1.jpg"),fit: BoxFit.cover,
-                )
               ),
-            
             ),
-          ),
-       const  Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Populer",style: TextStyle(fontSize: 18),),
-                 Text("view all",style: TextStyle(fontSize: 18),)
-              ],
+            Card(
+              color: Colors.black,
+              child: Container(
+                height: 150,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadiusDirectional.circular(10),
+                    image: const DecorationImage(
+                      image: AssetImage("image/home1.jpg"),
+                      fit: BoxFit.cover,
+                    )),
+              ),
             ),
-          ),
-           SmallGrid()
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Populer",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    "view all",
+                    style: TextStyle(fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+            SmallGrid()
           ],
         ),
       ),
